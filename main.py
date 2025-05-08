bb6e0131dcbd7be1838db53b012769474d5b0ee6
AlvaroPlantasur <a.pavon@plantasur.com>
2025-05-08 08:36:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -35,108 +35,95 @@ def main():
     start_date_str = start_date.strftime('%Y-%m-%d')
     
     # 3. Consulta SQL utilizando el rango dinámico de fechas
-    query = f"""
-    SELECT 
-        rc.name as "Compañía",
-        a.internal_number as "Número",
-        rp.vat as "CIF",
-        p.name AS "Cliente/Proveedor",
-        CASE 
-            WHEN a.type = 'out_invoice' THEN 'Cliente'
-            WHEN a.type = 'in_invoice' THEN 'Proveedor'
-            WHEN a.type = 'out_refund' THEN 'Rectificativa Cliente'
-            WHEN a.type = 'in_refund' THEN 'Rectificativa Proveedor'
-            ELSE 'Otro'
-        END as "Tipo",
- 
-        to_char(a.date_invoice, 'DD') as "Fecha Dia",
-        to_char(a.date_invoice, 'MM') as "Fecha Mes",
-        to_char(a.date_invoice, 'YYYY') as "Fecha Año",
-        to_char(a.date_invoice, 'DD/MM/YYYY') as "Fecha",
- 
-        afp.name as "Régimen fiscal",
- 
-        a.amount_untaxed_0 as "Base 0", 
-        a.amount_taxed_0 as "Importe 0", 
-        a.amount_total_0 as "Total 0",
- 
-        a.amount_untaxed_4 as "Base 4", 
-        a.amount_taxed_4 as "Importe 4", 
-        a.amount_total_4 as "Total 4",
- 
-        a.amount_untaxed_8 as "Base 8", 
-        a.amount_taxed_8 as "Importe 8", 
-        a.amount_total_8 as "Total 8",
- 
-        a.amount_untaxed_10 as "Base 10", 
-        a.amount_taxed_10 as "Importe 10", 
-        a.amount_total_10 as "Total 10",
- 
-        a.amount_untaxed_18 as "Base 18", 
-        a.amount_taxed_18 as "Importe 18", 
-        a.amount_total_18 as "Total 18",
- 
-        a.amount_untaxed_21 as "Base 21", 
-        a.amount_taxed_21 as "Importe 21", 
-        a.amount_total_21 as "Total 21",
- 
-        a.recargo_equivalencia_05 as "Recargo 0,5", 
-        a.recargo_equivalencia_1 as "Recargo 1", 
-        a.recargo_equivalencia_14 as "Recargo 1.4", 
-        a.recargo_equivalencia_4 as "Recargo 4",
-        a.recargo_equivalencia_52 as "Recargo 5.2",
- 
-        a.amount_total as "Total",
- 
-        (CASE 
-            WHEN a.type = 'out_refund' THEN
-                - (
-                    COALESCE(a.amount_untaxed_0, 0) +
-                    COALESCE(a.amount_untaxed_4, 0) +
-                    COALESCE(a.amount_untaxed_8, 0) +
-                    COALESCE(a.amount_untaxed_10, 0) +
-                    COALESCE(a.amount_untaxed_18, 0) +
-                    COALESCE(a.amount_untaxed_21, 0)
-                )
-            ELSE
-                (
-                    COALESCE(a.amount_untaxed_0, 0) +
-                    COALESCE(a.amount_untaxed_4, 0) +
-                    COALESCE(a.amount_untaxed_8, 0) +
-                    COALESCE(a.amount_untaxed_10, 0) +
-                    COALESCE(a.amount_untaxed_18, 0) +
-                    COALESCE(a.amount_untaxed_21, 0)
-                )
-         END) as "Total BASES",
-        CASE   
-            WHEN a.obsolescencia = TRUE THEN 'SI' 
-            ELSE 'NO' 
-        END as "BULK",
-        'S-' || rp.id AS "ID Cliente",
-        rpa.city AS "Ciudad"
- 
-    FROM 
-        account_invoice as a 
-    INNER JOIN 
-        res_partner as p ON p.id = a.partner_id 
-    INNER JOIN 
-        account_fiscal_position afp ON afp.id = a.fiscal_position 
-    INNER JOIN 
-        res_company rc ON rc.id = a.company_id
-    INNER JOIN 
-        res_partner rp ON rp.id = a.partner_id
-    INNER JOIN 
-        res_partner_address rpa ON rpa.id = a.address_invoice_id
- 
-    WHERE 
-        a.state IN ('open','paid') 
-        AND a.date_invoice BETWEEN '{start_date_str}' AND '{end_date_str}'
-        AND a.type IN ('out_invoice', 'out_refund')
-        AND a.internal_number NOT LIKE 'RA%'
- 
-    ORDER BY 
-        a.internal_number;
-    """
+    query = """
+SELECT 
+    rc.name as "Compañía",
+    a.internal_number as "Número",
+    rp.vat as "CIF",
+    p.name AS "Cliente/Proveedor",
+    CASE 
+        WHEN a.type = 'out_invoice' THEN 'Cliente'
+        WHEN a.type = 'in_invoice' THEN 'Proveedor'
+        WHEN a.type = 'out_refund' THEN 'Rectificativa Cliente'
+        WHEN a.type = 'in_refund' THEN 'Rectificativa Proveedor'
+        ELSE 'Otro'
+    END as "Tipo",
+    to_char(a.date_invoice, 'DD') as "Fecha Dia",
+    to_char(a.date_invoice, 'MM') as "Fecha Mes",
+    to_char(a.date_invoice, 'YYYY') as "Fecha Año",
+    to_char(a.date_invoice, 'DD/MM/YYYY') as "Fecha",
+    CASE 
+        WHEN afp.name = 'Recargo de Equivalencia' THEN 'Recargo de Equivalencia'
+        WHEN afp.name = 'Régimen Extracomunitario' THEN 'Régimen Extracomunitario'
+        WHEN afp.name IN ('REGIMEN INTRACOMUNITARIO', 'Régimen Intracomunitario') THEN 'Régimen Intracomunitario'
+        WHEN afp.name = 'REGIMEN NACIONAL' THEN 'Régimen Nacional'
+        ELSE afp.name
+    END as "Régimen fiscal",
+    a.amount_untaxed_0 as "Base 0", 
+    a.amount_taxed_0 as "Importe 0", 
+    a.amount_total_0 as "Total 0",
+    a.amount_untaxed_4 as "Base 4", 
+    a.amount_taxed_4 as "Importe 4", 
+    a.amount_total_4 as "Total 4",
+    a.amount_untaxed_8 as "Base 8", 
+    a.amount_taxed_8 as "Importe 8", 
+    a.amount_total_8 as "Total 8",
+    a.amount_untaxed_10 as "Base 10", 
+    a.amount_taxed_10 as "Importe 10", 
+    a.amount_total_10 as "Total 10",
+    a.amount_untaxed_18 as "Base 18", 
+    a.amount_taxed_18 as "Importe 18", 
+    a.amount_total_18 as "Total 18",
+    a.amount_untaxed_21 as "Base 21", 
+    a.amount_taxed_21 as "Importe 21", 
+    a.amount_total_21 as "Total 21",
+    a.recargo_equivalencia_05 as "Recargo 0,5", 
+    a.recargo_equivalencia_1 as "Recargo 1", 
+    a.recargo_equivalencia_14 as "Recargo 1.4", 
+    a.recargo_equivalencia_4 as "Recargo 4",
+    a.recargo_equivalencia_52 as "Recargo 5.2",
+    a.amount_total as "Total",
+    CASE 
+        WHEN a.type = 'out_refund' THEN 
+            - (
+                COALESCE(a.amount_untaxed_0, 0) +
+                COALESCE(a.amount_untaxed_4, 0) +
+                COALESCE(a.amount_untaxed_8, 0) +
+                COALESCE(a.amount_untaxed_10, 0) +
+                COALESCE(a.amount_untaxed_18, 0) +
+                COALESCE(a.amount_untaxed_21, 0)
+            )
+        ELSE 
+            (
+                COALESCE(a.amount_untaxed_0, 0) +
+                COALESCE(a.amount_untaxed_4, 0) +
+                COALESCE(a.amount_untaxed_8, 0) +
+                COALESCE(a.amount_untaxed_10, 0) +
+                COALESCE(a.amount_untaxed_18, 0) +
+                COALESCE(a.amount_untaxed_21, 0)
+            )
+    END as "Total BASES",
+    CASE   
+        WHEN a.obsolescencia = TRUE THEN 'SI' 
+        ELSE 'NO' 
+    END as "BULK",
+    'S-' || rp.id AS "ID Cliente",
+    rpa.city AS "Ciudad"
+FROM 
+    account_invoice as a 
+INNER JOIN res_partner as p ON p.id = a.partner_id 
+INNER JOIN account_fiscal_position afp ON afp.id = a.fiscal_position 
+INNER JOIN res_company rc ON rc.id = a.company_id
+INNER JOIN res_partner rp ON rp.id = a.partner_id
+INNER JOIN res_partner_address rpa ON rpa.id = a.address_invoice_id
+WHERE 
+    a.state IN ('open','paid') 
+    AND a.date_invoice BETWEEN %s AND %s
+    AND a.type IN ('out_invoice', 'out_refund')
+    AND a.internal_number NOT LIKE 'RA%'
+ORDER BY 
+    a.internal_number;
+"""
     
     # 4. Conectar a la base de datos, ejecutar la consulta y obtener los datos
     try:
